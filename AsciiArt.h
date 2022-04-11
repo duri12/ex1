f430fd6bd8ea26bd36adaa14f8fb1514a2851612
duri12 <eyalgor102@gmail.com>
2022-04-11 22:06:32 +0300

    added the map func only left  export_to_string

---
+++ AsciiArt.h
@@ -0,0 +1,35 @@
+//
+// Created by eyal on 11/04/2022.
+//
+
+#ifndef MTM_EX1_ASCIIART_H
+#define MTM_EX1_ASCIIART_H
+#include "RLEList.h"
+
+
+
+
+
+/**
+*   asciiArtRead: Reads the given file and returns its content encoded as a RLE list.
+*
+* @param in_stream - The file.
+* @return
+* 	RLEList that contains the content of the file.
+*/
+RLEList asciiArtRead(FILE* in_stream);
+
+
+/**
+*   asciiArtPrint: parse the image that is writen in a RLEList form
+*   and prints the result.
+* @param list - The List containing the image.
+* @param out_stream
+* @return
+* 	RLEList that contains the content of the file.
+*/
+RLEListResult asciiArtPrint(RLEList list, FILE *out_stream);
+
+
+
+#endif //MTM_EX1_ASCIIART_H
