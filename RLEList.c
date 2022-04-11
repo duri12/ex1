231d98853ed6fd69bb2bb479f8ab70656565bf93
duri12 <eyalgor102@gmail.com>
2022-04-11 19:27:49 +0300

    test

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,14 @@
 #include "RLEList.h"
 
-typedef struct {
-    //TODO: implement
+typedef struct
+{
+    int count;
+    char letter;
+    struct RLElist *node;
 } RLEList_t;
 
 //implement the functions here
+RLEList RLEListCreate()
+{
+
+}
