727873de80e831b00d2b8881bbd0d0559f7da932
duri12 <eyalgor102@gmail.com>
2022-04-12 17:52:20 +0300

    added ascii readfile thing

--- AsciiArt.c
+++ AsciiArt.c
@@ -1,4 +1,14 @@
 #include "RLEList.h"
 
-
+RLEList asciiArtRead(FILE* in_stream){
+    if(in_stream==NULL){
+        return NULL;
+    }
+    RLEList list =RLEListCreate();
+    char letter ;
+    while(fscanf(in_stream, "%c",&letter)){
+        RLEListAppend(list,letter);
+    }
+    return list;
+}
 
